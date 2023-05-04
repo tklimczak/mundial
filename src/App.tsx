364528f7a9a7e8d4081e3985bf70241fc43cc898
tklimczak <klimczak.tomek@gmail.com>
2023-05-04 19:57:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import { Tab } from "semantic-ui-react";
-import { GroupItem } from "./utils";
+import { createInitialGroups, GroupItem } from "./utils";
 import Eliminations from "./Eliminations";
 import Mundial from "./Mundial";
 
 function App() {
-  let initialGroup: GroupItem[] = [];
-
-  initialGroup.push({ name: "A", countries: ["gb", "it", "mx", "qa"] });
-  initialGroup.push({ name: "B", countries: ["ar", "is", "se", "us"] });
-  initialGroup.push({ name: "C", countries: ["fr", "es", "rs", "ir"] });
-  initialGroup.push({ name: "D", countries: ["be", "de", "ae", "fi"] });
-  initialGroup.push({ name: "E", countries: ["hr", "pl", "cz", "ca"] });
-  initialGroup.push({ name: "F", countries: ["br", "nl", "uy", "co"] });
-  initialGroup.push({ name: "G", countries: ["pt", "eg", "dk", "sn"] });
-  initialGroup.push({ name: "H", countries: ["dz", "ru", "no", "ne"] });
+  let initialGroup = createInitialGroups()
 
   const [groups, setGroups] = useState<GroupItem[]>(initialGroup);
 
