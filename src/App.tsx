dff007c1c7ad5c10efa9d29944907011fe8279be
tklimczak <klimczak.tomek@gmail.com>
2024-11-10 18:45:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,48 @@ import { Tab } from "semantic-ui-react";
 import { createInitialGroups, GroupItem } from "./utils";
 import Eliminations from "./Eliminations";
 import Mundial from "./Mundial";
+import SkiJumpingPlayers from "./skijumping/SkiJumpingPlayers";
+import { getPlayers } from "./skijumping/utils";
+import SkiJumpingHills from "./skijumping/SkiJumpingHills";
+import WorldCup from "./skijumping/WorldCup";
 
 function App() {
   let initialGroup = createInitialGroups()
+  let players = getPlayers();
 
   const [groups, setGroups] = useState<GroupItem[]>(initialGroup);
+  const [selectedPlayers, setPlayers] = useState<string[]>([]);
+  const [selectedHills, setHills] = useState<string[]>([]);
 
   const setMyGroups = (groups: GroupItem[]): void => {
     setGroups(groups);
   };
+
+  const changePlayerState=(name:string):void =>{
+    
+    let found = selectedPlayers.find(x=>x == name);
+    if (found) {
+      let newList  = selectedPlayers.filter(x=>x != name);
+      setPlayers([...newList]);
+    }  else {
+      let newList  = selectedPlayers;
+      newList.push(name);
+      setPlayers([...newList]);
+    }
+  }
+
+  const changeHillsState=(name:string):void =>{
+    
+    let found = selectedHills.find(x=>x == name);
+    if (found) {
+      let newList  = selectedHills.filter(x=>x != name);
+      setHills([...newList]);
+    }  else {
+      let newList  = selectedHills;
+      newList.push(name);
+      setHills([...newList]);
+    }
+  }
 
   const panes = [
     {
@@ -28,6 +61,24 @@ function App() {
       menuItem: "Munidal",
       render: () => <Tab.Pane attached={false}>
         <Mundial groups={groups} setGroups={setMyGroups}/>
+      </Tab.Pane>,
+    },
+    {
+      menuItem: "Lista Skoczkow",
+      render: () => <Tab.Pane attached={false}>
+        <SkiJumpingPlayers selectedPlayers={selectedPlayers} changePlayerState={changePlayerState}/>
+      </Tab.Pane>,
+    },
+    {
+      menuItem: "Lista Skoczni",
+      render: () => <Tab.Pane attached={false}>
+        <SkiJumpingHills selectedHills={selectedHills} changeHillsState={changeHillsState}/>
+      </Tab.Pane>,
+    },
+    {
+      menuItem: "Puchar Swiata",
+      render: () => <Tab.Pane attached={false}>
+        <WorldCup selectedHills={selectedHills} selectedPlayers={selectedPlayers} />
       </Tab.Pane>,
     },
   ];
